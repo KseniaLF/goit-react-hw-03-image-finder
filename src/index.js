88b2343b100eb26f96968dd04874bae5500340f0
KseniaLF <llf9391@gmail.com>
2023-03-02 09:50:17 +0200

    test comit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+
+import { GlobalStyle } from 'components/Constants/GlobalStyle';
+import { Layout } from 'components/Constants/Layout.styled';
+
+import { App } from 'components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <GlobalStyle />
+      <App />
+    </Layout>
   </React.StrictMode>
 );
