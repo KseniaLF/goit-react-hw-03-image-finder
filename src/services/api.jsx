2ee77d0b81061edab34329106ce3fd3220c36818
KseniaLF <llf9391@gmail.com>
2023-03-05 10:31:13 +0200

    fix per_page

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -5,7 +5,7 @@ const KEY = '24536039-c146a0cee4ea2e39b0ff8f9b5';
 
 export const getImgApiData = async (searchQuery, page) => {
   const response = await axios.get(
-    `/?q=${searchQuery}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=3`
+    `/?q=${searchQuery}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
 
   // console.log(response.data);
